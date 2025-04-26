969ce138a64b3666b8bc16513e8d0484ac2b2563
quan.hng <quan.hng@samsung.com>
2025-04-26 10:12:43 +0700

    test commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+// hihi haha
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
